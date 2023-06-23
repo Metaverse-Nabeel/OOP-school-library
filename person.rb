ae4c01b5a808563475d0d4286edd395d11db178e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-23 20:54:46 +0500

    Decorate class project completed

--- person.rb
+++ person.rb
@@ -1,15 +1,15 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :age, :name
   attr_reader :id
 
   def initialize(_id, age, parent_permission: true, name: 'unknown')
+    super(name)
     @id = Random.rand(1..1000)
     @age = age
     @parent_permission = parent_permission
-    @name = name
   end
-
-  private
 
   def of_age?
     @age >= 18
@@ -17,5 +17,9 @@ class Person
 
   def can_use_services?
     @age >= 18 || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
