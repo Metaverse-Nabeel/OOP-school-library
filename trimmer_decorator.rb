35bdfbcb2238d8698cd61904ff14166b7d65c654
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-24 10:34:34 +0500

    Changed the trimmed_name format

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
     trimmed_name = @nameable.correct_name[0...10]
-    trimmed_name.length == @nameable.correct_name.length ? trimmed_name : "#{trimmed_name}..."
+    trimmed_name.length == @nameable.correct_name.length ? trimmed_name : "#{trimmed_name}"
   end
 end
